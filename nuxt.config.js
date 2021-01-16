752b81dd5927b247c8e760aafac055ebdf8db7fb
m.abedini <7FfQjnAG2W6Bb8N>
2021-01-16 17:13:51 +0330

    Add assets like css and images

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
-    {src: '~/assets/sass/app.scss', lang: 'scss'}
+    {src: '~/assets/scss/app.scss', lang: 'scss'}
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
