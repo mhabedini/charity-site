94bcbb3acb676b1ae697f1b5abbb3093310a9aab
m.h.a.abedini@gmail.com <mohammadhoseinabedini@gmail.com>
2022-12-23 12:11:12 +0330

    feat: implement attachment file upload for household

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# bazarshahr-vendor
+# charity-vendor
 
 ## Build Setup
 
--- pages/households/_id.vue
+++ pages/households/_id.vue
@@ -20,7 +20,9 @@
           <b-list-group-item>آیا کاربر سید هست؟ : <span>{{ user.is_sadat ? 'بله' : 'خیر' }}</span></b-list-group-item>
           <b-list-group-item v-if="this.user.representative">معرف : <span>{{ user.representative }}</span>
           </b-list-group-item>
-          <b-list-group-item v-if="this.user.representative_mobile">شماره معرف : <span>{{ user.representative_mobile }}</span>
+          <b-list-group-item v-if="this.user.representative_mobile">شماره معرف : <span>{{
+              user.representative_mobile
+            }}</span>
           </b-list-group-item>
         </b-list-group>
 
@@ -37,7 +39,8 @@
             <h4 class="">اعضای خانواده</h4>
           </div>
           <div class="col-sm-6">
-            <nuxt-link :to="`/households/${household.id}/family-member/create`" class="btn mb-3 float-sm-right btn-danger mb-2"><i
+            <nuxt-link :to="`/households/${household.id}/family-member/create`"
+                       class="btn mb-3 float-sm-right btn-danger mb-2"><i
               class="mdi mdi-plus-circle mr-1"></i>افزودن عضو خانواده جدید
             </nuxt-link>
           </div>
@@ -60,8 +63,8 @@
                         <i class="mdi mdi-eye"></i></a>
                     </li>
                     <li class="list-inline-item">
-                      <nuxt-link :to="`/users/edit/${ data.item.id }`" class="action-icon">
-                        <i class="mdi mdi-square-edit-outline"></i></nuxt-link>
+                      <a href="javascript:void(0);" class="action-icon">
+                        <i class="mdi mdi-square-edit-outline"></i></a>
                     </li>
                     <li class="list-inline-item">
                       <a href="javascript:void(0);" class="action-icon">
@@ -73,6 +76,28 @@
 
             </span>
         </div>
+
+        <div class="card">
+          <div class="card-body">
+
+            <div class="row">
+              <div class="col-md-2 mt-2" v-for="attachment in attachments">
+                <img class="img-thumbnail" :src="attachment.path">
+              </div>
+            </div>
+
+            <h4 class="header-title mt-4">محل ذخیره فایلهای سرپرست</h4>
+            <!-- file upload -->
+            <vue-dropzone id="dropzone" ref="myVueDropzone"
+                          v-on:vdropzone-success="successfulUpdate"
+                          :use-custom-slot="true" :options="dropzoneOptions">
+              <div class="dz-message needsclick">
+                <i class="h1 text-muted ri-upload-cloud-2-line"></i>
+                <h3>فایل خود را اینجا رها کنید و یا برای انتخاب فایل کلیک کنید</h3>
+              </div>
+            </vue-dropzone>
+          </div>
+        </div>
       </div>
 
     </div>
@@ -80,7 +105,12 @@
 </template>
 
 <script>
+import vue2Dropzone from 'vue2-dropzone';
+
 export default {
+  components: {
+    vueDropzone: vue2Dropzone
+  },
   head() {
     return {
       title: `${this.title}`
@@ -92,6 +122,7 @@ export default {
       household: Object,
       user: Object,
       charityDepartment: Object,
+      attachments: Array,
       families: Array,
       totalRows: 1,
       currentPage: 1,
@@ -143,20 +174,60 @@ export default {
           key: "action",
           label: "عملیات"
         }
-      ]
-    }
+      ],
+      dropzoneOptions: {
+        url: `http://api.localhost/v1/households/${this.$route.params.id}/attachments`,
+        thumbnailWidth: 150,
+        maxFilesize: 4,
+        headers: {
+          'Authorization': this.$auth.$storage._state['_token.local']
+        },
+        previewTemplate: this.template(),
+      },
+    };
   },
   async asyncData({$axios, params}) {
     const household = await $axios.get(`/households/${params.id}`)
     return {
       household: household.data.data,
       user: household.data.data.user,
+      attachments: household.data.data.attachments,
       charityDepartment: household.data.data.charity_department,
       families: household.data.data.families
     }
   },
   mounted() {
-    this.user.birth_date = this.user.birth_date ? this.user.birth_date.split('T')[0] :  null
+    this.user.birth_date = this.user.birth_date ? this.user.birth_date.split('T')[0] : null
+  },
+  methods: {
+    successfulUpdate: function (file, response) {
+      this.$refs.myVueDropzone.removeFile(file);
+      this.attachments.push(response.data);
+    },
+    template: function () {
+      return ` <div class="dropzone-previews mt-3">
+            <div class="card mt-1 mb-0 shadow-none border">
+                <div class="p-2">
+                    <div class="row align-items-center">
+                        <div class="col-auto">
+                            <img data-dz-thumbnail src="#" class="avatar-sm rounded bg-light" alt="">
+                        </div>
+                        <div class="col pl-0">
+                            <a href="javascript:void(0);" class="text-muted font-weight-bold" data-dz-name></a>
+                            <p class="mb-0" data-dz-size></p>
+                        </div>
+                        <div class="col-auto">
+                            <!-- Button -->
+                            <a href="" class="btn btn-link btn-lg text-muted" data-dz-remove>
+                                <i class="fe-x"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    }
   }
 }
 </script>
